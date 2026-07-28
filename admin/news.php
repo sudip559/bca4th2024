032061b56d3e1fc9fe27542b6eccbae5620be986
Sudip Parajuli <sudip.parajulee@gmail.com>
2026-07-28 08:26:45 +0545

    Add news display functionality with image and description; create .gitignore for uploads

--- admin/news.php
+++ admin/news.php
@@ -1,3 +1,9 @@
+<?php 
+$qry = "SELECT * FROM news ORDER BY news_date DESC";
+include 'dbconnection.php';
+$result = mysqli_query($conn, $qry);
+include 'closeconnection.php';
+?>
 <?php include 'header.php'; ?>
     <h2 class="text-2xl font-bold">News</h2>
     <hr class="h-1 bg-red-500">
@@ -12,16 +18,21 @@
             <th class="border p-2 bg-blue-600 text-white">News Description</th>
             <th class="border p-2 bg-blue-600 text-white">Action</th>
         </tr>
+        <?php 
+        while($row = mysqli_fetch_assoc($result)){
+        ?>
         <tr class="text-center">
-            <td class="border p-2"></td>
-            <td class="border p-2"></td>
-            <td class="border p-2"></td>
-            <td class="border p-2"></td>
+            <td class="border p-2"><?php echo $row['news_date']; ?></td>
+            <td class="border p-2">
+                <img src="../uploads/<?php echo $row['photopath']; ?>" class="w-20 h-20 object-cover mx-auto" alt="News Image">
+            </td>
+            <td class="border p-2"><?php echo $row['title']; ?></td>
+            <td class="border p-2 line-clamp-4"><?php echo $row['description']; ?></td>
             <td class="border p-2">
                 <a href="" class="bg-blue-500 text-white rounded-lg px-2 py-1">Edit</a>
                 <a href="" onclick="return confirm('Are you sure you want to delete this category?');" class="bg-red-500 text-white rounded-lg px-2 py-1">Delete</a>
             </td>
         </tr>
-
+        <?php } ?>
     </table>
 <?php include 'footer.php'; ?>
